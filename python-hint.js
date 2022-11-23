49580dc185a2d44681bde5a03962841bf34beee4
Aditya Raj <adityaraj6508@gmail.com>
2022-11-24 03:04:53 +0530

    Added python hint v2

--- python-hint.js
+++ python-hint.js
@@ -43,19 +43,13 @@
     CodeMirror.pythonHint = pythonHint; // deprecated
     CodeMirror.registerHelper("hint", "python", pythonHint);
   
-    var pythonKeywords = "and del from not while as elif global or with assert else if pass yield"
-  + "break except import print class exec in raise continue finally is return def for lambda try";
+    var pythonKeywords = "and from not while as elif or else if pass"
+  + "break import print in continue return def for";
     var pythonKeywordsL = pythonKeywords.split(" ");
     var pythonKeywordsU = pythonKeywords.toUpperCase().split(" ");
   
-    var pythonBuiltins = "abs divmod input open staticmethod all enumerate int ord str "
-  + "any eval isinstance pow sum basestring execfile issubclass print super"
-  + "bin file iter property tuple bool filter len range type"
-  + "bytearray float list raw_input unichr callable format locals reduce unicode"
-  + "chr frozenset long reload vars classmethod getattr map repr xrange"
-  + "cmp globals max reversed zip compile hasattr memoryview round __import__"
-  + "complex hash min set apply delattr help next setattr buffer"
-  + "dict hex object slice coerce dir id oct sorted intern ";
+    var pythonBuiltins = "input print range "
+  + "moveUp moveDown moveLeft moveRight";
     var pythonBuiltinsL = pythonBuiltins.split(" ").join("() ").split(" ");
     var pythonBuiltinsU = pythonBuiltins.toUpperCase().split(" ").join("() ").split(" ");
   
