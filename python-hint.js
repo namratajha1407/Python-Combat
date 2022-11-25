821df5d3882ef4eef6f68ca8185bae2b9477310b
namratajha1407 <98533170+namratajha1407@users.noreply.github.com>
2022-11-26 01:15:47 +0530

    Update python-hint.js

--- python-hint.js
+++ python-hint.js
@@ -49,7 +49,7 @@
     var pythonKeywordsU = pythonKeywords.toUpperCase().split(" ");
   
     var pythonBuiltins = "print range "
-  + "moveUp moveDown moveLeft moveRight attack enemy unveil_friend input";
+  + "moveUp moveDown moveLeft moveRight attack enemy unveil_friend note_name input";
     var pythonBuiltinsL = pythonBuiltins.split(" ").join("() ").split(" ");
     var pythonBuiltinsU = pythonBuiltins.toUpperCase().split(" ").join("() ").split(" ");
   
