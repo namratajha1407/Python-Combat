18a49758c6b0001dddd0495e3b1ab4e4d9ecc46b
namratajha1407 <98533170+namratajha1407@users.noreply.github.com>
2022-11-25 20:38:13 +0530

    Level 4

---
+++ script3.js
@@ -0,0 +1,130 @@
+var canvas = document.getElementById('myCanvas');
+var context = canvas.getContext('2d');
+var villain_img = document.getElementById('villain1');
+
+var cHeight = canvas.height;
+var cWidth = canvas.width;
+
+var xTiles = 4;
+var yTiles = 4;
+
+function tile(x, y, width, height, color){
+    this.x = x;
+    this.y = y;
+    this.height = height;
+    this.width = width;
+    this.color = color;
+    this.allowed = true;
+    this.enemy = false;
+    this.friend = false;
+
+    this.draw = function(){
+        context.beginPath();
+        context.fillStyle = this.color;
+        context.fillRect(this.x*this.width, this.y*this.height, this.width, this.height);
+        context.fill();
+        context.closePath();
+    }
+
+    this.not_allowed = function(){
+        if(this.allowed){
+            return false;
+        }
+        return true;
+    }
+
+    this.is_enemy = function(){
+        return this.enemy;
+    }
+
+    this.is_friend = function(){
+        return this.friend; 
+    }
+}
+
+var Width = cWidth/xTiles;
+var Height = cHeight/yTiles;
+
+a = Math.floor(Math.random()*(2))+1; // 1 or 2
+b = Math.floor(Math.random()*(4));
+
+p=1;
+q=b;
+
+var tiles = new Array(xTiles);
+for (let i = 0; i < xTiles; i++) {
+    tiles[i] = new Array(yTiles);
+    for (let j = 0; j < yTiles; j++) {
+        if ((i+j)%2){
+            tiles[i][j] = new tile(i, j, Width, Height, "red");
+        }
+        else{
+            tiles[i][j] = new tile(i, j, Width, Height, "blue");
+        }    
+    tiles[i][j].draw();
+    }
+}
+
+function villain(x, y, tile_width, tile_height){
+    this.x = x;
+    this.y = y;
+    this.height = tile_height/2;
+    this.width = tile_width/2;
+
+    this.draw = function(){
+        context.beginPath()
+        context.drawImage(villain_img, this.x*tile_width + tile_width/4, this.y*tile_height + tile_height/4, this.width, this.height);
+        context.closePath()
+    }
+}
+
+var n = 3;
+let k = 0;
+var n_villain = 0;
+
+createArena = function(Tile_Height, Tile_Width){
+    for(let k=0; k<n; k++){
+        var j = k+1;
+        var i = Math.floor(Math.random()*(3))+1;
+        if(i!=xTiles-1 || j!=yTiles-1){   
+            v = new villain(i,j,Tile_Width, Tile_Height);
+            v.draw();
+            tiles[i][j].allowed = false;
+            if((i+j) % 2==0){
+                tiles[i][j].enemy = true;
+                n_villain++;
+            }
+            else{
+                tiles[i][j].friend = true;
+            }
+        }
+        else{
+            k--;
+        }
+    }
+}
+createArena(Height, Width);
+
+success = function(){
+    var game = document.getElementById("game_area");
+    game.style.opacity = 0.5;
+    //show "Well done! Next Level" button, which loads the next page
+    var success_button = document.getElementById("success");
+    success_button.style.display = "block";
+}
+
+fail = function(){
+    var game = document.getElementById("game_area");
+    game.style.opacity = 0.5;
+    //show "Try Again!" button, which reloads the page
+    var fail_button = document.getElementById("fail");
+    fail_button.style.display = "block";
+}
+
+document.getElementById("success").onclick = function(){
+    location.href = "index4.html"
+};
+
+document.getElementById("fail").onclick = function(){
+    location.reload()
+};
